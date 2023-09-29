eccea28648e2cea353ab3788d5f0643af482457d
Deemah26 <144944397+Deemah26@users.noreply.github.com>
2023-09-29 09:52:51 +0000

    Update LinkedList.java

--- DSproject/src/LinkedList.java
+++ DSproject/src/LinkedList.java
@@ -1,158 +1,68 @@
-public class LinkedList{ 
-     
-  private Contact Head; 
-  private Contact current; 
-   
-  public LinkedList(){ 
-      Head=current=null; 
-  } 
-   
-  public boolean Empty(){ 
-      return Head==null; 
-  } 
-  public boolean Full(){ 
-      return false; 
-  } 
-  public boolean Last(){ 
-      return current.next==null; 
-  } 
-  public void FindFirst(){ 
-      current=Head; 
-  } 
-  public void FindNext(){ 
-      current=current.next; 
-  } 
-  public void UpdateName(String name){ 
-      current.setContactName(name); 
-  } 
-  public void UpdatePhoneNumber(String phoneNumber){ 
-      current.setPhoneNumber(phoneNumber); 
-  } 
-  public void UpdateEmailAddress(String EmailAddress){ 
-      current.setEmailAddress(EmailAddress); 
-  } 
-   public void UpdateAddress(String Address){ 
-      current.setAddress(Address); 
-  } 
+package dataStructure;
+
+public class LinkedList <T>{ 
     
-   public void UpdateBirthday(String Birthday){ 
-      current.setBirthday(Birthday); 
-  } 
-    public void UpdateNotes(String Notes){ 
-      current.setNotes(Notes); 
-  } 
-     
-    public String RetriveName(){ 
-        return current.getContactName(); 
-    } 
-    public String RetrivePhoneNumber(){ 
-        return current.getPhoneNumber(); 
-    } 
-    public String RetriveEmailAddress(){ 
-        return current.getEmailAddress(); 
-    } 
-    public String RetriveAddress(){ 
-        return current.getAddress(); 
-    } 
-    public String RetriveBirthday(){ 
-        return current.getBirthday(); 
-    } 
-    public String RetriveNotes(){ 
-        return current.getNotes(); 
-    } 
-    public void insert(String Contactname,String PhoneNumber,String EmailAddress,String Address,String Birthday,String Notes ){ 
-        Contact p=new Contact(Contactname,PhoneNumber,EmailAddress,Address,Birthday,Notes); 
-        if (Empty()){ 
-            current=Head=p;   
-        } 
-        else{ 
-            p.next=current.next; 
-            current.next=p; 
-            current=p;} 
-             
-        } 
-    public void remove(){ 
-        if(current==Head){ 
-          Head=Head.next; 
-        } 
-         
-        else{ 
-            Contact p=Head; 
-            while(p.next!= current) 
-                p=p.next; 
-             
-            p.next=current.next; 
-        } 
-            if(current.next==null) 
-                current=Head; 
-         
-            else 
-                current=current.next; 
-                 
-        } 
-         
-    public LinkedList SearchByName(String name){ 
-            LinkedList list =new LinkedList(); 
-            Contact temp=Head; 
-            while(temp!=null){ 
-                if(temp.getContactName().equals(name)) 
-                    list.insert(temp.getContactName(), temp.getPhoneNumber(), temp.emailAddress, temp.getAddress(), temp.getBirthday(), temp.getNotes()); 
-                    temp=temp.next; 
-            } 
-             
-             
-           return list;  
-             
-        } 
-    public LinkedList SearchByPhoneNumber(String PhoneNumber){ 
-            LinkedList list =new LinkedList(); 
-            Contact temp=Head; 
-            while(temp!=null){ 
-                if(temp.getContactName().equals(PhoneNumber)) 
-                    list.insert(temp.getContactName(), temp.getPhoneNumber(), temp.emailAddress, temp.getAddress(), temp.getBirthday(), temp.getNotes()); 
-                    temp=temp.next; 
-            } 
-             
-             
-           return list;  
-             
-        } 
-    public LinkedList SearchByEmailAddress(String EmailAddress){ 
-            LinkedList list =new LinkedList(); 
-            Contact temp=Head; 
-            while(temp!=null){ 
-                if(temp.getEmailAddress().equals(EmailAddress)) 
-                    list.insert(temp.getContactName(), temp.getPhoneNumber(), temp.emailAddress, temp.getAddress(), temp.getBirthday(), temp.getNotes()); 
-                    temp=temp.next; 
-            } 
-             
-             
-           return list;  
-             
-        } 
-    public LinkedList SearchByAddress(String Address){ 
-        LinkedList list =new LinkedList(); 
-        Contact temp=Head; 
-        while(temp!=null){ 
-            if(temp.getAddress().equals(Address)) 
-                list.insert(temp.getContactName(), temp.getPhoneNumber(), temp.emailAddress, temp.getAddress(), temp.getBirthday(), temp.getNotes()); 
-                temp=temp.next; 
-        } 
-         
-         
-       return list;  
-         
-    } 
-public LinkedList SearchByBirthday(String Birthday){ 
-        LinkedList list =new LinkedList(); 
-        Contact temp=Head; 
-        while(temp!=null){ 
-            if(temp.getBirthday().equals(Birthday)) 
-                list.insert(temp.getContactName(), temp.getPhoneNumber(), temp.emailAddress, temp.getAddress(), temp.getBirthday(), temp.getNotes()); 
-                temp=temp.next; 
-        } 
-         
-         
-       return list;  
-         
-    }}
+	  private Node<T> Head; 
+	  private Node<T> current; 
+	   
+	  public LinkedList(){ 
+	      Head=current=null; 
+	  } 
+	   
+	  public boolean Empty(){ 
+	      return Head == null; 
+	  } 
+	  public boolean Full(){ 
+	      return false; 
+	  } 
+	  public boolean Last(){ 
+	      return current.next == null; 
+	  } 
+	  public void FindFirst(){ 
+	      current = Head; 
+	  } 
+	  public void FindNext(){ 
+	      current = current.next; 
+	  } 
+	  public void Update(T d){ 
+	      current.data = d; 
+	  } 
+	  
+	    public T Retrive(){ 
+	        return current.data; 
+	    } 
+	 
+	    public void Insert(T d) {
+	    
+	    	Node<T> tmp;
+	    	if (Empty()) {
+	    		current = Head = new Node<T> (d);
+	    	}
+	    	else {
+	    		tmp = current.next;
+	    		current.next = new Node<T> (d);
+	    		current = current.next;
+	    		current.next = tmp;
+	    	}
+	    }
+	    
+	    	public void Remove () {
+	    		
+	    		if (current == Head) {
+	    			Head = Head.next;
+	    		}
+	    		else {
+	    			Node<T> tmp = Head;
+	    			while (tmp.next != current)
+	    				tmp = tmp.next;
+	    			tmp.next = current.next;
+	    		}
+	    		if (current.next == null)
+	    			current = Head;
+	    		else
+	    			current = current.next;
+	    	}
+	    
+	    
+	         
+	    }
