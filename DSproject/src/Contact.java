f7856512421f5971786a468b88888836ca90dbb8
Deemah26 <144944397+Deemah26@users.noreply.github.com>
2023-09-29 09:51:02 +0000

    Update Contact.java

--- DSproject/src/Contact.java
+++ DSproject/src/Contact.java
@@ -1,77 +1,73 @@
-public class Contact{ 
- public String ContactName; 
- public String phoneNumber; 
- public String emailAddress; 
- public String address; 
- public String birthday; 
- public String notes; 
- public Contact next; 
- 
-    public Contact(String ContactName, String phoneNumber, String emailAddress, String address, String birthday, String notes) { 
-        this.ContactName = ContactName; 
-        this.phoneNumber = phoneNumber; 
-        this.emailAddress = emailAddress; 
-        this.address = address; 
-        this.birthday = birthday; 
-        this.notes = notes; 
-        this.next=null; 
-    } 
-    public void setNext(Contact next){ 
-       this.next=next;  
-         
-    } 
-     
-    public Contact getNext(){ 
-        return next; 
-    } 
- 
-    public String getContactName() { 
-        return ContactName ; 
-    } 
- 
-    public void setContactName(String name) { 
-        ContactName = name; 
-    } 
- 
-    public String getPhoneNumber() { 
-        return phoneNumber; 
-    } 
- 
-    public void setPhoneNumber(String phoneNumber) { 
-        this.phoneNumber = phoneNumber; 
-    } 
- 
-    public String getEmailAddress() { 
-        return emailAddress; 
-    } 
- 
-    public void setEmailAddress(String emailAddress) { 
-        this.emailAddress = emailAddress; 
-    } 
- 
-    public String getAddress() { 
-        return address; 
-    } 
- 
-    public void setAddress(String address) { 
-        this.address = address; 
-    } 
- 
-    public String getBirthday() { 
-        return birthday; 
-    } 
- 
-    public void setBirthday(String birthday) { 
-        this.birthday = birthday; 
-    } 
- 
-    public String getNotes() { 
-        return notes; 
-    } 
- 
-    public void setNotes(String notes) { 
-        this.notes = notes; 
-    } 
+package dataStructure;
+
+public class Contact <T> { 
+	 public T ContactName; 
+	 public T phoneNumber; 
+	 public T emailAddress; 
+	 public T address; 
+	 public T birthday; 
+	 public T notes; 
+	 
+	 
+	    public Contact(T ContactName, T phoneNumber, T emailAddress, T address, T birthday, T notes) { 
+	        this.ContactName = ContactName; 
+	        this.phoneNumber = phoneNumber; 
+	        this.emailAddress = emailAddress; 
+	        this.address = address; 
+	        this.birthday = birthday; 
+	        this.notes = notes; 
+	        
+	    } 
+	   
+	    public T getContactName() { 
+	        return ContactName ; 
+	    } 
+	 
+	    public void setContactName(T name) { 
+	        ContactName = name; 
+	    } 
+	 
+	    public T getPhoneNumber() { 
+	        return phoneNumber; 
+	    } 
+	 
+	    public void setPhoneNumber(T phoneNumber) { 
+	        this.phoneNumber = phoneNumber; 
+	    } 
+	 
+	    public T getEmailAddress() { 
+	        return emailAddress; 
+	    } 
+	 
+	    public void setEmailAddress(T emailAddress) { 
+	        this.emailAddress = emailAddress; 
+	    } 
+	 
+	    public T getAddress() { 
+	        return address; 
+	    } 
+	 
+	    public void setAddress(T address) { 
+	        this.address = address; 
+	    } 
+	 
+	    public T getBirthday() { 
+	        return birthday; 
+	    } 
+	 
+	    public void setBirthday(T birthday) { 
+	        this.birthday = birthday; 
+	    } 
+	 
+	    public T getNotes() { 
+	        return notes; 
+	    } 
+	 
+	    public void setNotes(T notes) { 
+	        this.notes = notes; 
+	    } 
+	     
+	     
      
      
      
