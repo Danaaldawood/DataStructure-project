d586d161535dcfab2cece9177d1ea4c1ca1e242f
Jumanahali <144927134+Jumanahali@users.noreply.github.com>
2023-10-16 22:15:35 +0300

    Update Event.java

--- DSproject/src/Event.java
+++ DSproject/src/Event.java
@@ -1,14 +1,62 @@
 
-public class Event { 
+package phonebook;
+
+public class Event implements Comparable<String> {
+   
 public String Title; 
 public String Date; 
 public String Time; 
-public String Location; 
+public String Location;
+public Contact contact;
+
+
  
  
-    public Event(String Title, String Date, String Time, String Location) { 
+    public Event(String Title, String Date, String Time, String Location,Contact c) { 
         this.Title = Title; 
         this.Date = Date; 
         this.Time = Time; 
-        this.Location = Location; 
-    }}
+        this.Location = Location;
+        contact=c;
+        
+    }
+
+    @Override
+    public String toString() {
+        return "Event{" + "Title=" + Title + ", Date=" + Date + ", Time=" + Time + ", Location=" + Location + ", contact name=" + contact.ContactName + '}';
+    }
+    
+
+    public int CompareTo( String c){
+            
+           return Title.compareTo(c);
+           }  
+
+
+
+
+
+}
+
+
+    
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
