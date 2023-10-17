e2ab506a7549e2f362a293f2c7d43ca6ad1b9b8f
Jumanahali <144927134+Jumanahali@users.noreply.github.com>
2023-10-17 15:25:48 +0300

    Update Contact.java

--- DSproject/src/Contact.java
+++ DSproject/src/Contact.java
@@ -13,93 +13,94 @@ public class Contact implements Comparable<String>{
             
 	 
 	    
-	    public Contact(String ContactName, String phoneNumber, String emailAddress, String address, String birthday, String notes) { 
-	        this.ContactName = ContactName; 
-	        this.phoneNumber = phoneNumber; 
-	        this.emailAddress = emailAddress; 
-	        this.address = address; 
-	        this.birthday = birthday; 
-	        this.notes = notes;
-                ListEvent=new LinkedList<Event>(); 
+	 public Contact(String ContactName, String phoneNumber, String emailAddress, String address, String birthday, String notes) { 
+	    this.ContactName = ContactName; 
+	    this.phoneNumber = phoneNumber; 
+	    this.emailAddress = emailAddress; 
+	    this.address = address; 
+	    this.birthday = birthday; 
+	    this.notes = notes;
+            ListEvent=new LinkedList<Event>(); 
 	        
 	    } 
 	   
-	    public String getContactName() { 
-	        return ContactName ; 
+	public String getContactName() { 
+	    return ContactName ; 
 	    } 
 	 
-	    public void setContactName(String name) { 
-	        ContactName = name; 
+        public void setContactName(String name) { 
+	     ContactName = name; 
 	    } 
 	 
-	    public String getPhoneNumber() { 
-	        return phoneNumber; 
+	public String getPhoneNumber() { 
+	    return phoneNumber; 
 	    } 
 	 
-	    public void setPhoneNumber(String phoneNumber) { 
-	        this.phoneNumber = phoneNumber; 
+	public void setPhoneNumber(String phoneNumber) { 
+	    this.phoneNumber = phoneNumber; 
 	    } 
 	 
-	    public String getEmailAddress() { 
-	        return emailAddress; 
+	public String getEmailAddress() { 
+	    return emailAddress; 
 	    } 
 	 
-	    public void setEmailAddress(String emailAddress) { 
-	        this.emailAddress = emailAddress; 
+	public void setEmailAddress(String emailAddress) { 
+	    this.emailAddress = emailAddress; 
 	    } 
 	 
-	    public String getAddress() { 
-	        return address; 
+	public String getAddress() { 
+	    return address; 
 	    } 
 	 
-	    public void setAddress(String address) { 
-	        this.address = address; 
+	public void setAddress(String address) { 
+	    this.address = address; 
 	    } 
 	 
-	    public String getBirthday() { 
-	        return birthday; 
+	public String getBirthday() { 
+	    return birthday; 
 	    } 
 	 
-	    public void setBirthday(String birthday) { 
-	        this.birthday = birthday; 
+	public void setBirthday(String birthday) { 
+	    this.birthday = birthday; 
 	    } 
 	 
-	    public String getNotes() { 
-	        return notes; 
+	public String getNotes() { 
+	    return notes; 
 	    } 
 	 
-	    public void setNotes(String notes) { 
-	        this.notes = notes; 
+	public void setNotes(String notes) { 
+	   this.notes = notes; 
 	    } 
             
-            public LinkedList<Event> getEvent(){
-                return ListEvent;
+        public LinkedList<Event> getEvent(){
+            return ListEvent;
             }
+        //method CompareTo receive String contact name c for the contact node in the list and compare it with the contact name for the new contact to be added and return integer to indicate the suitable arrangment for the contact node depending on the contact name.
+        public int CompareTo(String c){
             
-            public int CompareTo(String c){
-            
-           return ContactName.compareTo(ContactName);
+           return ContactName.compareTo(c);
            }  
 
     @Override
-    public String toString() {
-        return "Contact{" + "ContactName=" + ContactName + ", phoneNumber=" + phoneNumber + ", emailAddress=" + emailAddress + ", address=" + address + ", birthday=" + birthday + ", notes=" + notes + '}';
+        public String toString() {
+           return "Contact{" + "ContactName=" + ContactName + ", phoneNumber=" + phoneNumber + ", emailAddress=" + emailAddress + ", address=" + address + ", birthday=" + birthday + ", notes=" + notes + '}';
     }
-            
-     public  boolean addEvent(Event e){
-         if (! ListEvent.Empty())
+    
+        //method addEvent receive Event object e and check the Date and Time of it if there is any conflict with other Event Date and Time(equal Date and Time for the Event e with other Event object in the  ListEvent list) return false if there is any conflict,if there is no conflict add it to the ListEvent and return true. 
+        public  boolean addEvent(Event e){
+           if (! ListEvent.Empty())
         {
-            ListEvent.FindFirst();
-           while(! ListEvent.Last() )
+                ListEvent.FindFirst();
+                while(! ListEvent.Last() )
             {
-                if ((ListEvent.Retrive().Date.compareTo(e.Date) == 0) 
+                    if ((ListEvent.Retrive().Date.compareTo(e.Date) == 0) 
                         && (ListEvent.Retrive().Time.compareTo(e.Time) == 0))
                     return false;
                 
-                ListEvent.FindNext();
+                    ListEvent.FindNext();
             }
            
-           if ((ListEvent.Retrive().Date.compareTo(e.Date) == 0) 
+                if ((ListEvent.Retrive().Date.compareTo(e.Date) == 0) 
                         && (ListEvent.Retrive().Time.compareTo(e.Time) == 0))
                     return false;
            
@@ -110,11 +111,11 @@ public class Contact implements Comparable<String>{
                 
             
      }       
-                
-      public void RemoveEvents(){
-          if(!ListEvent.Empty()){
-              ListEvent.FindFirst();
-              while(!ListEvent.Last()){
+       //method RemoveEvents will remove all the Nodes in the ListEvent.          
+        public void RemoveEvents(){
+           if(!ListEvent.Empty()){
+               ListEvent.FindFirst();
+               while(!ListEvent.Last()){
                     ListEvent.Remove();
               }
                ListEvent.Remove();
@@ -132,4 +133,5 @@ public class Contact implements Comparable<String>{
     
     
 }
+
 
